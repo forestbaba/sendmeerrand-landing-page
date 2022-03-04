972431ff58bf530bcc7361dc758e56323d4198c2
forestbaba <hawlahethan@gmail.com>
2022-03-04 17:59:52 +0100

    Fix 1

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,7 @@ import { Link } from 'react-scroll';
 
 export default function Header() {
     const [active, setActive] = useState("5") 
+    const [open, setOpen] = useState(false) 
     const features =[{id:"1",name:"Home"}, 
     {id:"2",name:"Feature"},
     {id:"4",name:"FAQ"},
@@ -11,9 +12,13 @@ export default function Header() {
     {id:"5",name:"Get the App"}]
   return (
     <div className='flex-column mx-10 my-5 justify-center flex-wrap justify-between
-     md:flex sm:flex-column'>
-        <img src={Icon} alt="icon" width={100} height={30} className="justify-center"/>
-        <ul  className='flex-column items-center md:flex md:justify-center sm:flex-column'>
+     md:flex sm:flex-column'> 
+         <div>
+         <img src={Icon} alt="icon" width={100} height={30} className="justify-center"/>
+         <button onClick={()=> setOpen(!open)}><i  className= {!open ? "fa fa-bars fa-2x absolute top-0 right-0 px-5 py-8 md:hidden ease-in duration-300 " :"fa fa-times fa-2x absolute top-0 right-0 px-5 py-8 md:hidden ease-in duration-300 "} aria-hidden="true" style={{ color:'#F77E00' }}></i></button>
+
+         </div>
+        <ul  className={!open ? 'flex-column items-center md:flex md:justify-center sm:flex-column hidden ease-out duration-300' :'flex-column items-center md:flex md:justify-center sm:flex-column ease-in duration-300 '}>
             {features.map(item => <li className='py-5 md:py-0'>
             <Link activeClass={"3"} 
             to={item.id} 
@@ -22,7 +27,7 @@ export default function Header() {
             duration={500} 
             className='px-5 font-bold text-center'
             onClick={()=> setActive(item.id)}
-             style={{ backgroundColor:active === item.id ? "#F77F00" : null,color:active===item.id && 'white', padding:20, borderRadius:"15px" }}>{item.name}</Link></li>)}
+             style={{ backgroundColor:active === item.id ? "#F77F00" : null,color:active===item.id && 'white', padding:20, borderRadius:"15px", borderBottom: item.id === "5" ? "1px solid #F77F00" : null }}>{item.name}</Link></li>)}
         </ul>
     </div>
   )
--- src/components/page4.js
+++ src/components/page4.js
@@ -1,35 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Page4() {
-  let google="google"
-  let apple=""
-  let items =[{id:1,name:"What is ‘send me errand’?", desc:"Send me errand is an online need example foodstuff, grocery etc. platform accessible via our mobile app, where you can order and have them delivered to your doorstep."},
-  {id:2,name:"How much does it cost to buy food?", desc:"he more you buy the lesser. Send me errand charges only a percent for your total purchase."},
-  {id:3,name:"How can I pay for food purchased?",desc:" You can make payments online via your debit card. The process is fast and simple."},
-  {id:4,name:"Can I make orders over the telephone?", desc:`No! Send me errand only entertains orders via her mobile application available on <a href=${google}>Google playstore</a> and  <a href=${apple}>Apple app store</a>`},
-  {id:5,name:"Can I use the platform to make orders anytime of the day?",desc:"Yes! Send me errand entertains orders 24/7"}]
+  const [s, setS] = useState(-1)
+  let google = "google"
+  let apple = ""
+  let items = [{ id: 1, name: "What is ‘send me errand’?", desc: "Send me errand is an online need example foodstuff, grocery etc. platform accessible via our mobile app, where you can order and have them delivered to your doorstep." },
+  { id: 2, name: "How much does it cost to buy food?", desc: "he more you buy the lesser. Send me errand charges only a percent for your total purchase." },
+  { id: 3, name: "How can I pay for food purchased?", desc: " You can make payments online via your debit card. The process is fast and simple." },
+  { id: 4, name: "Can I make orders over the telephone?", desc: `No! Send me errand only entertains orders via her mobile application available on <a href=${google}>Google playstore</a> and  <a href=${apple}>Apple app store</a>` },
+  { id: 5, name: "Can I use the platform to make orders anytime of the day?", desc: "Yes! Send me errand entertains orders 24/7" }]
   return (
     <div className='bg-black px-5 text-center text-white' id='4'>
-        <h1 className='font-bold text-xl md:text-3xl pt-5'>Frequently asked questions</h1>
-        <p className='font-light'>Here are the few Frequently asked questions people asks</p>
-        <div  className='flex-column items-start px-5 py-5 md:px-40 md:py-20 content-start divide-y'>
-          {
-            items.map(item =>{
-              return(
-                <div className='flex-column items-start content-start '>
-                <h3 className='text-left font-bold md:pt-10'>{item.name}</h3>
-                {item.id !== 4 ?(<p className='text-left font-light md:pb-8 '>{item.desc}</p>) :
-                ( <p className='text-left font-light md:pb-8 '>No! Send me errand only entertains orders via her mobile application available on <a href="">Google playstore</a> and  <a href="">Apple app store</a></p>)}
-               
+      <h1 className='font-bold text-xl md:text-3xl pt-5'>Frequently asked questions</h1>
+      <p className='font-light'>Here are the few Frequently asked questions people asks</p>
+      <div className='flex-column items-start px-5 py-5 md:px-40 md:py-20 content-start divide-y'>
+        {
+          items.map((item, index) => {
+            return (
+              <div className='flex-column items-start content-start'>
+                <div className='flex justify-between'
+                  onClick={() => setS(item.id)}>
+                  <h3 className='text-left font-bold md:pt-10'
+                  >{item.name}
+                  </h3>
+                  { s === item.id ? (<i className="fa fa-angle-down fa-2x md:pt-7"></i>) : (<i className="fa fa-angle-right fa-2x md:pt-7"></i>)}
+                 
+                </div>
+                {s === item.id ? (<>
+                  {item.id !== 4 ? (<p className='text-left font-light md:pb-8 '>{item.desc}</p>) :
+                    (<p className='text-left font-light md:pb-8 '>No! Send me errand only entertains orders via her mobile application available on <a href="">Google playstore</a> and  <a href="">Apple app store</a></p>)}
+                </>) : null
+                }
+
 
               </div>
-              )
-            })
-          }
-        
-         
-          
-        </div>
+            )
+          })
+        }
+
+
+
+      </div>
     </div>
   )
 }
