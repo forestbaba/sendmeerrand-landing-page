a5f195b91c5fa16bb646d6c90029eba3f830dede
forestbaba <hawlahethan@gmail.com>
2022-03-06 10:49:01 +0100

    updates

--- src/components/page1.js
+++ src/components/page1.js
@@ -5,6 +5,7 @@ import MG from "../asset/mg.svg";
 import Down from "../asset/download.svg";
 
 export default function Page1() {
+
   return (
     <div
       className="flex-column md:flex px-20 py-10"
@@ -19,21 +20,21 @@ export default function Page1() {
       <div className="mr-30">
         <img src={Down} alt="google" className="mb-7 w-40 md:w-72" />
         <h1
-          className="-ml-4 md:-ml-32 font-bold text-3xl md:text-4xl"
+          className="-ml-4 md:-ml-20 font-bold text-3xl md:text-4xl"
         >
           Get started with
         </h1>
         <h1
-          className="-ml-4 md:-ml-32 pb-5 font-bold text-3xl md:text-4xl"
+          className="-ml-4 md:-ml-20 pb-5 font-bold text-3xl md:text-4xl"
         >
           Send Me Errand
         </h1>
         <div className="flex" style={{ display: "" }}>
-          <img src={P2} alt="google" className="w-32 md:w-44"/>
-          <img src={P1} alt="apple"  className="w-32 md:w-44 ml-2 md:ml-10 lg:ml-10" />
+          <img src={P2} alt="google" className="w-32  md:w-36"/>
+          <img src={P1} alt="apple"  className="w-32 md:w-36 ml-2 md:ml-10 lg:ml-10" />
         </div>
       </div>
-      <img src={MG} alt="mg"  className="min-height-700" />
+      <img src={MG} alt="mg"  className="w-88 md:w-64 lg:w-80" />
     </div>
   );
 }
